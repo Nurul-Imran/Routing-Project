5653b4bb270563c38516be65f3cb3f921ff98d50
Nurul Imran <nurulimran696@gmail.com>
2024-07-12 19:34:03 +0600

    Update h1 in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <div>
             <PageTitle title="Home" />
-            <h1>Home page</h1>
+            <h1>Home</h1>
             <div>
                 <h3>Blog 1</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, voluptatem eveniet! Rem suscipit, quam officiis sint iure dolorum temporibus totam eveniet magnam sunt architecto eligendi cupiditate consectetur nemo, asperiores esse excepturi exercitationem repellendus nostrum! Vitae repudiandae, laborum expedita doloremque hic tenetur ipsum repellat sapiente sint error cupiditate laboriosam voluptas tempora.</p>
